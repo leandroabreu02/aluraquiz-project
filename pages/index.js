cd89eb6a169f63899b8e9c50d47099a86d8dc52d
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-01-26 15:55:27 -0300

    Imersão React aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -24,15 +29,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo/>
         <Widget>
           <Widget.Header>
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p> Lorem ipsum dolor sit amet ...</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+    
+              router.push(`/quiz?name=${name}`);
+              // router manda para a próxima página
+            }}
+            >
+              <input
+                onChange={function(infosDoEnvento) {
+                  // state
+                  // name = infosDoEnvento.target.value;
+                  setName(infosDoEnvento.target.value);
+                }}
+                placeholder="Qual seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
